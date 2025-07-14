f9648bb0030990a182ebf4cd9fca5e8e8bd3117c
Aktiar0403 <aktiardocuments@gmail.com>
2025-07-15 01:23:08 +0530

    Added accordion layout

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log("NephroCare Pro App Loaded.");
+console.log("NephroCare Pro App Loaded.");
+
+const buttons = document.querySelectorAll('.accordion-button');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    button.classList.toggle('active');
+    const content = button.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+});
